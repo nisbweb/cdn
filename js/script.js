6de6783ada6bf3704c3b4810bb05a997c0e91f90
mridulganga <mridul.kepler@gmail.com>
2016-10-01 20:09:06 +0530

    Adding new navbar js and css

--- js/script.js
+++ js/script.js
@@ -15,6 +15,17 @@ $(window).load(function() {
 
 $(document).ready(function() {
 
+        $('a[href="#navbar-more-show"], .navbar-more-overlay').on('click', function(event) {
+		event.preventDefault();
+		$('body').toggleClass('navbar-more-show');
+		if ($('body').hasClass('navbar-more-show'))	{
+			$('a[href="#navbar-more-show"]').closest('li').addClass('active');
+		}else{
+			$('a[href="#navbar-more-show"]').closest('li').removeClass('active');
+		}
+		return false;
+	    });
+
         $('.dropdown-toggle').dropdown()
 
 		$('.nav-tabs a').click(function(){
